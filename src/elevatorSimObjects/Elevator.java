7ea596359162b852925b6776875c44dd57991692
mrdinklepus <mrdinklepus@yahoo.com>
2015-11-08 23:45:06 -0700

    Still working on adding elevator internal functionality

--- src/elevatorSimObjects/Elevator.java
+++ src/elevatorSimObjects/Elevator.java
@@ -8,15 +8,38 @@ public class Elevator
   private int totalTrips = 0;
   private int currentFloorNumber = 1;  // Assume elevators all start at floor 1
   private int floorsPassedThisTrip = 0;
+  private int maxFloorNum;
   
-  public Elevator(BuildingController controller)
+  // addedStop
+  private int floorToStopAt;
+  
+  public Elevator(BuildingController controller, int maxFloorNum)
   {
     this.controller = controller;
+    this.maxFloorNum = maxFloorNum;
   }
   
+  /**
+   * This constitutes starting a 'trip'
+   * This should be done in its own thread independent of other elevators
+   */
   public void moveToFloor(int floorNum)
   {
+    if (floorNum < 1 || floorNum > maxFloorNum)
+    {
+      // Create this exception
+      throw FloorOutOfBoundsException("Floor Number is out of range!");
+    }
     
+    int floorDiff = floorNum - currentFloorNumber;
+    for (int i = currentFloorNumber; i <= floorNum; i++)
+    {
+      if (floorToStopAt == 0)
+      {
+        currentFloorNumber++;
+        reportFloorChange();
+      }
+    }
   }
   
   public void reportFloorChange()
@@ -24,5 +47,8 @@ public class Elevator
     controller.elevatorReport(this, currentFloorNumber);
   }
   
-  
+  public void addStopAtFloor(int floorNum)
+  {
+    
+  }
 }
